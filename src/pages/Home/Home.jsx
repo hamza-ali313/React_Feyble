1bbe833e5045cbb6f366b3ea42df20169d2efa26
0SyedAli <sa8449006@gmail.com>
2024-02-21 04:26:57 +0500

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,11 @@ import { FaUser } from "react-icons/fa6";
 import landinglogo from '../../assets/Images/landing-logo.png'
 import { Link } from "react-router-dom";
 import { CiMenuFries } from "react-icons/ci";
-import landinglogo from '../../assets/Images/landing-logo.png'
+// import landinglogo from '../../assets/Images/landing-logo.png'
 import { Container, Row, Col, Dropdown } from "react-bootstrap";
 import './home.css'
 function Home() {
+  
   return (
     <div>
       <section className="landing-pg">
